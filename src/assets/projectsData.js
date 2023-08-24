4d0adfd6c253fd8bb545b6d54a2c9ab30573f204
Ikaro <ikarochagas.ig@gmail.com>
2023-08-24 13:21:09 -0300

    Update projectsData.js

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -8,7 +8,7 @@ export const projectsData = [
     {
         projectName: 'Site Pessoal',
         projectDescription: 'Site pessoal para cololocar os projetos produzidos',
-        imageUrl: '',
-        projectUrl: '',
+        imageUrl: 'https://private-user-images.githubusercontent.com/100325726/263037717-4763f192-bd3c-4679-9bdf-77dc480a3a14.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTI4OTQxODUsIm5iZiI6MTY5Mjg5Mzg4NSwicGF0aCI6Ii8xMDAzMjU3MjYvMjYzMDM3NzE3LTQ3NjNmMTkyLWJkM2MtNDY3OS05YmRmLTc3ZGM0ODBhM2ExNC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwODI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDgyNFQxNjE4MDVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT01MTA5MzMwYzU3ZTc0NDg4OGE3YjNjOTA2ZmRmMTkyNDU2NjAyZTJkOWRmZDI3MDQ5YTViOGFiYWM2MzhiM2U3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.JgxbkHAOs4H6_DSaNV6-hY2a_m_XBOtNypgHhmgiYPA',
+        projectUrl: 'https://personal-website-gray-beta.vercel.app/',
     },
 ];
