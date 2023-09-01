47f3a6e6bcfd458bf35136d991bef9c4f22ef0ab
Íkaro Chagas <100325726+IkaroChagas@users.noreply.github.com>
2023-09-01 11:09:34 -0300

    Update projectsData.js

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -3,7 +3,7 @@ export const projectsData = [
         projectName: 'Pluto',
         projectDescription: 'O Projeto Pluto é uma iniciativa que visa simular uma entidade de doações voltada para crianças com necessidades em diversas unidades ao redor do Brasil. O objetivo central do projeto é proporcionar uma plataforma onde seja possível criar, editar e excluir unidades de doação, simplificando assim a gestão e operação do processo de doações.',
         imageUrl: 'https://user-images.githubusercontent.com/100325726/262744350-b8d11057-0f37-4a03-bd85-4d528b175cc3.png',
-        projectUrl: 'https://pluto-react-front-5wqp.vercel.app/',
+        projectUrl: 'pluto-react-front-5wqp-ikarochagas.vercel.app',
     },
     {
         projectName: 'Site Pessoal',
